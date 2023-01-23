e5049925ab0bef85c3965b33dcf773a68331082a
mnthfdn <43201159+mnthfdn@users.noreply.github.com>
2023-01-23 13:41:45 +0300

    responsive settings

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-jQuery(".slick-arrow").css("display","none"),jQuery(document).ready(function(){jQuery(".slick-arrow").css("display","block"),jQuery(".cardContainer").slick({infinite:!0,slidesToShow:2.5,slidesToScroll:1,prevArrow:jQuery(".left"),nextArrow:jQuery(".right"),responsive:[{breakpoint:768,settings:{arrows:!1}},]}),jQuery(".color").hover(function(){jQuery(this).parent().find(".color.active").removeClass("active"),jQuery(this).addClass("active")},function(){jQuery(this).removeClass("active"),jQuery(this).parent().find(".color").eq(3).addClass("active")})});
+jQuery(document).ready(function () {
+  
+  function waitSlick() {
+    
+    if (jQuery.fn.slick) {
+      jQuery('.slick-arrow').css('border', '1px solid rgba(0, 0, 0, 0.1)')
+      jQuery(".slick-arrow").css("display", "block");
+      jQuery(".cardContainer").slick({
+        prevArrow: jQuery(".left"),
+        nextArrow: jQuery(".right"),
+        infinite: !0,
+        slidesToShow: 2.5,
+        slidesToScroll: 1,
+        responsive: [{ breakpoint: 768, settings: { arrows: !1 } }],
+      });
+    } else {
+      setTimeout(waitSlick(), 100);
+    }
+  }
+
+  waitSlick()
+
+  jQuery(".color").hover(
+    function () {
+      jQuery(this).parent().find(".color.active").removeClass("active"),
+        jQuery(this).addClass("active");
+    },
+    function () {
+     // jQuery(this).removeClass("active");
+      // jQuery(this).parent().find(".color").eq(3).addClass("active");
+    }
+  );
+});
